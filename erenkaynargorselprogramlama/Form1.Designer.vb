becc460e70e7242a6b86cbbfe8f59431b29afd5e
erenkaynarr <erenbabaerenos@gmail.com>
2022-06-02 22:28:23 +0300

    forth commit

---
+++ erenkaynargorselprogramlama/Form1.Designer.vb
@@ -0,0 +1,286 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.btnMULTIPLY = New System.Windows.Forms.Button()
+        Me.btnEQUALS = New System.Windows.Forms.Button()
+        Me.btnDOT = New System.Windows.Forms.Button()
+        Me.btn0 = New System.Windows.Forms.Button()
+        Me.btnSLASH = New System.Windows.Forms.Button()
+        Me.btn3 = New System.Windows.Forms.Button()
+        Me.btn2 = New System.Windows.Forms.Button()
+        Me.btn1 = New System.Windows.Forms.Button()
+        Me.btnMINUS = New System.Windows.Forms.Button()
+        Me.btn6 = New System.Windows.Forms.Button()
+        Me.btn5 = New System.Windows.Forms.Button()
+        Me.btn4 = New System.Windows.Forms.Button()
+        Me.btnPLUS = New System.Windows.Forms.Button()
+        Me.btn9 = New System.Windows.Forms.Button()
+        Me.btn8 = New System.Windows.Forms.Button()
+        Me.btn7 = New System.Windows.Forms.Button()
+        Me.text1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnMULTIPLY
+        '
+        Me.btnMULTIPLY.Font = New System.Drawing.Font("Segoe UI", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btnMULTIPLY.Location = New System.Drawing.Point(352, 308)
+        Me.btnMULTIPLY.Name = "btnMULTIPLY"
+        Me.btnMULTIPLY.Size = New System.Drawing.Size(100, 78)
+        Me.btnMULTIPLY.TabIndex = 62
+        Me.btnMULTIPLY.Text = "*"
+        Me.btnMULTIPLY.UseVisualStyleBackColor = True
+        '
+        'btnEQUALS
+        '
+        Me.btnEQUALS.Font = New System.Drawing.Font("Segoe UI", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btnEQUALS.Location = New System.Drawing.Point(246, 392)
+        Me.btnEQUALS.Name = "btnEQUALS"
+        Me.btnEQUALS.Size = New System.Drawing.Size(100, 78)
+        Me.btnEQUALS.TabIndex = 61
+        Me.btnEQUALS.Text = "="
+        Me.btnEQUALS.UseVisualStyleBackColor = True
+        '
+        'btnDOT
+        '
+        Me.btnDOT.Font = New System.Drawing.Font("Segoe UI", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btnDOT.Location = New System.Drawing.Point(140, 392)
+        Me.btnDOT.Name = "btnDOT"
+        Me.btnDOT.Size = New System.Drawing.Size(100, 78)
+        Me.btnDOT.TabIndex = 60
+        Me.btnDOT.Text = "."
+        Me.btnDOT.UseVisualStyleBackColor = True
+        '
+        'btn0
+        '
+        Me.btn0.Font = New System.Drawing.Font("Segoe UI", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btn0.Location = New System.Drawing.Point(34, 392)
+        Me.btn0.Name = "btn0"
+        Me.btn0.Size = New System.Drawing.Size(100, 78)
+        Me.btn0.TabIndex = 59
+        Me.btn0.Text = "0"
+        Me.btn0.UseVisualStyleBackColor = True
+        '
+        'btnSLASH
+        '
+        Me.btnSLASH.Font = New System.Drawing.Font("Segoe UI", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btnSLASH.Location = New System.Drawing.Point(352, 224)
+        Me.btnSLASH.Name = "btnSLASH"
+        Me.btnSLASH.Size = New System.Drawing.Size(100, 78)
+        Me.btnSLASH.TabIndex = 58
+        Me.btnSLASH.Text = "/"
+        Me.btnSLASH.UseVisualStyleBackColor = True
+        '
+        'btn3
+        '
+        Me.btn3.Font = New System.Drawing.Font("Segoe UI", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btn3.Location = New System.Drawing.Point(246, 308)
+        Me.btn3.Name = "btn3"
+        Me.btn3.Size = New System.Drawing.Size(100, 78)
+        Me.btn3.TabIndex = 57
+        Me.btn3.Text = "3"
+        Me.btn3.UseVisualStyleBackColor = True
+        '
+        'btn2
+        '
+        Me.btn2.Font = New System.Drawing.Font("Segoe UI", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btn2.Location = New System.Drawing.Point(140, 308)
+        Me.btn2.Name = "btn2"
+        Me.btn2.Size = New System.Drawing.Size(100, 78)
+        Me.btn2.TabIndex = 56
+        Me.btn2.Text = "2"
+        Me.btn2.UseVisualStyleBackColor = True
+        '
+        'btn1
+        '
+        Me.btn1.Font = New System.Drawing.Font("Segoe UI", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btn1.Location = New System.Drawing.Point(34, 308)
+        Me.btn1.Name = "btn1"
+        Me.btn1.Size = New System.Drawing.Size(100, 78)
+        Me.btn1.TabIndex = 55
+        Me.btn1.Text = "1"
+        Me.btn1.UseVisualStyleBackColor = True
+        '
+        'btnMINUS
+        '
+        Me.btnMINUS.Font = New System.Drawing.Font("Segoe UI", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btnMINUS.Location = New System.Drawing.Point(352, 392)
+        Me.btnMINUS.Name = "btnMINUS"
+        Me.btnMINUS.Size = New System.Drawing.Size(100, 78)
+        Me.btnMINUS.TabIndex = 54
+        Me.btnMINUS.Text = "-"
+        Me.btnMINUS.UseVisualStyleBackColor = True
+        '
+        'btn6
+        '
+        Me.btn6.Font = New System.Drawing.Font("Segoe UI", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btn6.Location = New System.Drawing.Point(246, 224)
+        Me.btn6.Name = "btn6"
+        Me.btn6.Size = New System.Drawing.Size(100, 78)
+        Me.btn6.TabIndex = 53
+        Me.btn6.Text = "6"
+        Me.btn6.UseVisualStyleBackColor = True
+        '
+        'btn5
+        '
+        Me.btn5.Font = New System.Drawing.Font("Segoe UI", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btn5.Location = New System.Drawing.Point(140, 224)
+        Me.btn5.Name = "btn5"
+        Me.btn5.Size = New System.Drawing.Size(100, 78)
+        Me.btn5.TabIndex = 52
+        Me.btn5.Text = "5"
+        Me.btn5.UseVisualStyleBackColor = True
+        '
+        'btn4
+        '
+        Me.btn4.Font = New System.Drawing.Font("Segoe UI", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btn4.Location = New System.Drawing.Point(34, 224)
+        Me.btn4.Name = "btn4"
+        Me.btn4.Size = New System.Drawing.Size(100, 78)
+        Me.btn4.TabIndex = 51
+        Me.btn4.Text = "4"
+        Me.btn4.UseVisualStyleBackColor = True
+        '
+        'btnPLUS
+        '
+        Me.btnPLUS.Font = New System.Drawing.Font("Segoe UI", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btnPLUS.Location = New System.Drawing.Point(352, 140)
+        Me.btnPLUS.Name = "btnPLUS"
+        Me.btnPLUS.Size = New System.Drawing.Size(100, 78)
+        Me.btnPLUS.TabIndex = 50
+        Me.btnPLUS.Text = "+"
+        Me.btnPLUS.UseVisualStyleBackColor = True
+        '
+        'btn9
+        '
+        Me.btn9.Font = New System.Drawing.Font("Segoe UI", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btn9.Location = New System.Drawing.Point(246, 140)
+        Me.btn9.Name = "btn9"
+        Me.btn9.Size = New System.Drawing.Size(100, 78)
+        Me.btn9.TabIndex = 49
+        Me.btn9.Text = "9"
+        Me.btn9.UseVisualStyleBackColor = True
+        '
+        'btn8
+        '
+        Me.btn8.Font = New System.Drawing.Font("Segoe UI", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btn8.Location = New System.Drawing.Point(140, 140)
+        Me.btn8.Name = "btn8"
+        Me.btn8.Size = New System.Drawing.Size(100, 78)
+        Me.btn8.TabIndex = 48
+        Me.btn8.Text = "8"
+        Me.btn8.UseVisualStyleBackColor = True
+        '
+        'btn7
+        '
+        Me.btn7.Font = New System.Drawing.Font("Segoe UI", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btn7.Location = New System.Drawing.Point(34, 140)
+        Me.btn7.Name = "btn7"
+        Me.btn7.Size = New System.Drawing.Size(100, 78)
+        Me.btn7.TabIndex = 47
+        Me.btn7.Text = "7"
+        Me.btn7.UseVisualStyleBackColor = True
+        '
+        'text1
+        '
+        Me.text1.Font = New System.Drawing.Font("Segoe UI", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.text1.Location = New System.Drawing.Point(81, 11)
+        Me.text1.Multiline = True
+        Me.text1.Name = "text1"
+        Me.text1.Size = New System.Drawing.Size(319, 45)
+        Me.text1.TabIndex = 42
+        Me.text1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Segoe UI", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Button1.Location = New System.Drawing.Point(34, 482)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(312, 78)
+        Me.Button1.TabIndex = 63
+        Me.Button1.Text = "CLEAR"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("Wingdings", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Button2.Location = New System.Drawing.Point(352, 482)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(100, 78)
+        Me.Button2.TabIndex = 64
+        Me.Button2.Text = ""
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(519, 580)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.btnMULTIPLY)
+        Me.Controls.Add(Me.btnEQUALS)
+        Me.Controls.Add(Me.btnDOT)
+        Me.Controls.Add(Me.btn0)
+        Me.Controls.Add(Me.btnSLASH)
+        Me.Controls.Add(Me.btn3)
+        Me.Controls.Add(Me.btn2)
+        Me.Controls.Add(Me.btn1)
+        Me.Controls.Add(Me.btnMINUS)
+        Me.Controls.Add(Me.btn6)
+        Me.Controls.Add(Me.btn5)
+        Me.Controls.Add(Me.btn4)
+        Me.Controls.Add(Me.btnPLUS)
+        Me.Controls.Add(Me.btn9)
+        Me.Controls.Add(Me.btn8)
+        Me.Controls.Add(Me.btn7)
+        Me.Controls.Add(Me.text1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnMULTIPLY As Button
+    Friend WithEvents btnEQUALS As Button
+    Friend WithEvents btnDOT As Button
+    Friend WithEvents btn0 As Button
+    Friend WithEvents btnSLASH As Button
+    Friend WithEvents btn3 As Button
+    Friend WithEvents btn2 As Button
+    Friend WithEvents btn1 As Button
+    Friend WithEvents btnMINUS As Button
+    Friend WithEvents btn6 As Button
+    Friend WithEvents btn5 As Button
+    Friend WithEvents btn4 As Button
+    Friend WithEvents btnPLUS As Button
+    Friend WithEvents btn9 As Button
+    Friend WithEvents btn8 As Button
+    Friend WithEvents btn7 As Button
+    Friend WithEvents text1 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+End Class
---
+++ erenkaynargorselprogramlama/Form1.vb
@@ -0,0 +1,103 @@
+﻿Public Class Form1
+    Dim fnum As Double
+    Dim snum As Double
+    Dim sign As String
+
+    Private Sub btn0_Click(sender As Object, e As EventArgs) Handles btn0.Click
+        text1.Text = text1.Text & 0
+    End Sub
+
+    Private Sub btn1_Click(sender As Object, e As EventArgs) Handles btn1.Click
+        text1.Text = text1.Text & 1
+    End Sub
+
+    Private Sub btn2_Click(sender As Object, e As EventArgs) Handles btn2.Click
+        text1.Text = text1.Text & 2
+    End Sub
+
+    Private Sub btn3_Click(sender As Object, e As EventArgs) Handles btn3.Click
+        text1.Text = text1.Text & 3
+    End Sub
+
+    Private Sub btn4_Click(sender As Object, e As EventArgs) Handles btn4.Click
+        text1.Text = text1.Text & 4
+    End Sub
+
+    Private Sub btn5_Click(sender As Object, e As EventArgs) Handles btn5.Click
+        text1.Text = text1.Text & 5
+    End Sub
+
+    Private Sub btn6_Click(sender As Object, e As EventArgs) Handles btn6.Click
+        text1.Text = text1.Text & 6
+    End Sub
+
+    Private Sub btn7_Click(sender As Object, e As EventArgs) Handles btn7.Click
+        text1.Text = text1.Text & 7
+    End Sub
+
+    Private Sub btn8_Click(sender As Object, e As EventArgs) Handles btn8.Click
+        text1.Text = text1.Text & 8
+    End Sub
+
+    Private Sub btn9_Click(sender As Object, e As EventArgs) Handles btn9.Click
+        text1.Text = text1.Text & 9
+    End Sub
+
+    Private Sub btnDOT_Click(sender As Object, e As EventArgs) Handles btnDOT.Click
+        text1.Text = text1.Text & "."
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        text1.Text = ""
+    End Sub
+
+    Private Sub btnPLUS_Click(sender As Object, e As EventArgs) Handles btnPLUS.Click
+        fnum = text1.Text
+        sign = "+"
+        text1.Text = ""
+    End Sub
+
+    Private Sub btnSLASH_Click(sender As Object, e As EventArgs) Handles btnSLASH.Click
+        fnum = text1.Text
+        sign = "/"
+        text1.Text = ""
+    End Sub
+
+    Private Sub btnMULTIPLY_Click(sender As Object, e As EventArgs) Handles btnMULTIPLY.Click
+        fnum = text1.Text
+        sign = "*"
+        text1.Text = ""
+    End Sub
+
+    Private Sub btnMINUS_Click(sender As Object, e As EventArgs) Handles btnMINUS.Click
+        fnum = text1.Text
+        sign = "-"
+        text1.Text = ""
+    End Sub
+
+    Private Sub btnEQUALS_Click(sender As Object, e As EventArgs) Handles btnEQUALS.Click
+        snum = text1.Text
+        If sign = "+" Then
+            text1.Text = fnum + snum
+        ElseIf sign = "/" Then
+            text1.Text = fnum / snum
+        ElseIf sign = "*" Then
+            text1.Text = fnum * snum
+        ElseIf sign = "-" Then
+            text1.Text = fnum - snum
+        End If
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If text1.Text.Length > 0 Then
+            text1.Text = text1.Text.Remove(text1.Text.Length - 1, 1)
+        End If
+
+        If text1.Text = "" Then
+            text1.Text = ""
+        End If
+
+
+    End Sub
+End Class
